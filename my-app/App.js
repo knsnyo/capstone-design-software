9e9817c1b1a8dc1646eb5ea99a568d3b659db97a
knsnyo <isc963@naver.com>
2022-05-07 18:11:52 +0900

    20220507 refreshcontrol test

--- my-app/App.js
+++ my-app/App.js
@@ -11,7 +11,7 @@ export default () => {
   return (
     <NavigationContainer>
       <Stack.Navigator 
-      initialRouteName="Result"
+      initialRouteName="Home"
       screenOptions={{cardStyle: {backgroundColor: "#fff"}}}>
         <Stack.Screen name="Home" component={Home}/>
         <Stack.Screen name="Search" component={Search}/>
--- my-app/component/ItemList/ItemList.jsx
+++ my-app/component/ItemList/ItemList.jsx
@@ -1,4 +1,5 @@
-import { View, FlatList } from "react-native";
+import { useState, useCallback } from "react";
+import { View, FlatList, RefreshControl } from "react-native";
 import Item from "../Item/Item";
 import { styles } from "./style";
 
@@ -22,12 +23,27 @@ export default () => {
     { id: 16, title: "나나나" },
   ];
 
+  const [refreshing, setRefreshing] = useState(false);
+  const wait = (timeout) => {
+    return new Promise((resolve) => {
+      return setTimeout(resolve, timeout);
+    });
+  };
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    wait(2000).then(() => {
+      setRefreshing(false);
+    });
+  }, []);
   return (
     <View style={styles.container}>
       <FlatList
         data={data}
         renderItem={({ item }) => <Item key={item.id} title={item.title} />}
         numColumns={3}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
       />
     </View>
   );
